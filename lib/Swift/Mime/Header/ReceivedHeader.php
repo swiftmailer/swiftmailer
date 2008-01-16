b33e1102f7e16f8b42d2bc27dad8e58a23fe25c7
d11wtq <d11wtq>
2008-01-16 07:05:45 +0000

    Started implementing Received header field.

--- lib/Swift/Mime/Header/ReceivedHeader.php
+++ lib/Swift/Mime/Header/ReceivedHeader.php
@@ -18,7 +18,7 @@
  
  */
 
-require_once dirname(__FILE__) . '/StructuredHeader.php';
+require_once dirname(__FILE__) . '/DateHeader.php';
 require_once dirname(__FILE__) . '/../HeaderEncoder.php';
 
 /**
@@ -27,21 +27,126 @@ require_once dirname(__FILE__) . '/../HeaderEncoder.php';
  * @subpackage Mime
  * @author Chris Corbyn
  */
-class Swift_Mime_Header_ReceivedHeader
-  extends Swift_Mime_Header_StructuredHeader
+class Swift_Mime_Header_ReceivedHeader extends Swift_Mime_Header_DateHeader
 {
+  
+  private $_data = array();
+  private $_pairsPerLine = 2;
   
   /**
    * Creates a new ReceivedHeader with the given $name and $info.
    * @param string $name
-   * @param string[] $info, optional
+   * @param int $timestamp, optional
+   * @param string[] $data, optional
    * @param string $charset, optional
    * @param Swift_Mime_HeaderEncoder $encoder, optional
    */
-  public function __construct($name, $info = array(), $charset = null,
-    Swift_Mime_HeaderEncoder $encoder = null)
+  public function __construct($name, $timestamp = null, $data = array(),
+    $charset = null, Swift_Mime_HeaderEncoder $encoder = null)
   {
-    parent::__construct($name, null, $charset, $encoder);
+    parent::__construct($name, $timestamp, $charset, $encoder);
+    $this->setData($data);
+  }
+  
+  /**
+   * Set name value pair data in the Header.
+   * This method takes a list of associative arrays, each of which containing
+   * the keys 'name' and 'value', and optionally a 'comment' key.
+   * Example:
+   * <code>
+   * <?php
+   * $header->setData(array(
+   *  array('name' => 'by', 'value' => 'server.tld'),
+   *  array('name' => 'with', 'value' => 'ESMTP', 'comment' => 'Using SupaDupaMail 1.3')
+   *  ));
+   * ?>
+   * </code>
+   * @param array $data
+   * @see setPairsPerLine()
+   * @see setTimestamp()
+   * @see setValue()
+   */
+  public function setData(array $data)
+  {
+    foreach ($data as &$nvp)
+    {
+      if (!array_key_exists('comment', $nvp))
+      {
+        $nvp['comment'] = null;
+      }
+    }
+    $this->_data = $data;
+  }
+  
+  /**
+   * Get the list of name-value pairs in the Header.
+   * The returned value is an array of associative arrays, each of which
+   * contains the keys 'name', 'value' and 'comment'.
+   * A comment with a value of NULL means it's not set.
+   * @return array
+   * @see setData()
+   * @see getTimestamp()
+   * @see getValue()
+   */
+  public function getData()
+  {
+    return $this->_data;
+  }
+  
+  /**
+   * Set the number of name-value pairs which can appear on any same line.
+   * The default is two (a reasonable default).
+   * WARNING: If lines are longer than the value returned by {@link getLineLength()}
+   * folding whitespace will be added.
+   * @param int $num
+   */
+  public function setPairsPerLine($num)
+  {
+    $this->_pairsPerLine = (int) $num;
+  }
+  
+  /**
+   * Get the maximum number of name-value paris which may appear on any one line.
+   * @return int
+   * @see setPairsPerLine()
+   */
+  public function getPairsPerLine()
+  {
+    return $this->_pairsPerLine;
+  }
+  
+  /**
+   * Get the string value of the body in this Header.
+   * This is not necessarily RFC 2822 compliant since folding white space will
+   * not be added at this stage (see {@link toString()} for that).
+   * @return string
+   * @see toString()
+   */
+  public function getValue()
+  {
+    $nvpStrings = array();
+    $currentPairs = array();
+    foreach ($this->_data as $i => $nvp)
+    {
+      //Force a new line if reached max number of name-value pairs on this line
+      if ((0 != $i) && 0 == ($i % $this->_pairsPerLine))
+      {
+        $nvpStrings[] = implode(' ', $currentPairs);
+        $currentPairs = array();
+      }
+      
+      $nvpString = $nvp['name'] . ' ' . $nvp['value'];
+      if (!is_null($nvp['comment']))
+      {
+        $nvpString .= ' (' . $nvp['comment'] . ')';
+      }
+      $currentPairs[] = $nvpString;
+    }
+    if (!empty($currentPairs))
+    {
+      $nvpStrings[] = implode(' ', $currentPairs);
+    }
+    return implode("\r\n ", $nvpStrings) . '; ' . parent::getValue();
   }
   
 }
--- tests/unit/Swift/Mime/Header/ReceivedHeaderTest.php
+++ tests/unit/Swift/Mime/Header/ReceivedHeaderTest.php
@@ -1,14 +1,7 @@
 <?php
 
 require_once 'Swift/Mime/Header/ReceivedHeader.php';
-require_once 'Swift/Mime/HeaderAttribute.php';
-require_once 'Swift/Mime/HeaderAttributeSet.php';
-require_once 'Swift/Mime/HeaderEncoder.php';
 
-Mock::generate('Swift_Mime_HeaderAttribute', 'Swift_Mime_MockHeaderAttribute');
-Mock::generate('Swift_Mime_HeaderAttributeSet',
-  'Swift_Mime_MockHeaderAttributeSet'
-  );
 Mock::generate('Swift_Mime_HeaderEncoder', 'Swift_Mime_MockHeaderEncoder');
 
 class Swift_Mime_Header_ReceivedHeaderTest extends UnitTestCase
@@ -16,21 +9,127 @@ class Swift_Mime_Header_ReceivedHeaderTest extends UnitTestCase
   
   private $_charset = 'utf-8';
   
-  public function testNothing()
+  public function testTimestampCanBeSetAndFetched()
   {
-    $this->assertFalse(true, 'Nothing');
+    $timestamp = time();
+    $header = $this->_getHeader('Received', $timestamp);
+    $this->assertEqual($timestamp, $header->getTimestamp());
+  }
+  
+  public function testTimestampCanBeSetBySetter()
+  {
+    $timestamp = time();
+    $header = $this->_getHeader('Received');
+    $header->setTimestamp($timestamp);
+    $this->assertEqual($timestamp, $header->getTimestamp());
+  }
+  
+  public function testDateIsRenderedAccordingToRfc2822()
+  {
+    /* -- RFC 2822, 3.6.7.
+       The "Received:" field contains a
+       (possibly empty) list of name/value pairs followed by a semicolon and
+       a date-time specification.  The first item of the name/value pair is
+       defined by item-name, and the second item is either an addr-spec, an
+       atom, a domain, or a msg-id.  Further restrictions may be applied to
+       the syntax of the trace fields by standards that provide for their
+       use, such as [RFC2821].
+       */
+    
+    $timestamp = time();
+    $header = $this->_getHeader('Received', $timestamp);
+    $this->assertEqual('; ' . date('r', $timestamp), $header->getValue());
+  }
+  
+  public function testNameValuePairsCanBeSet()
+  {
+    /* -- RFC 2822, 3.6.7.
+      name-val-list   =       [CFWS] [name-val-pair *(CFWS name-val-pair)]
+
+      name-val-pair   =       item-name CFWS item-value
+
+      item-name       =       ALPHA *(["-"] (ALPHA / DIGIT))
+
+      item-value      =       1*angle-addr / addr-spec /
+                               atom / domain / msg-id
+    */
+    
+    $timestamp = time();
+    $header = $this->_getHeader('Received', $timestamp,
+      array(
+        array('name' => 'from', 'value' => 'mx1.googlemail.com')
+        )
+      );
+    $this->assertEqual('from mx1.googlemail.com; ' . date('r', $timestamp),
+      $header->getValue()
+      );
+  }
+  
+  public function testUsingMultipleNameValuePairs()
+  {
+    $timestamp = time();
+    $header = $this->_getHeader('Received', $timestamp,
+      array(
+        array('name' => 'from', 'value' => 'mx1.googlemail.com'),
+        array('name' => 'by', 'value' => 'mail.swiftmailer.org')
+        )
+      );
+    $this->assertEqual(
+      'from mx1.googlemail.com by mail.swiftmailer.org; ' . date('r', $timestamp),
+      $header->getValue()
+      );
+  }
+  
+  public function testLongSetsOfNameValuePairsCanBeSplitIntoLines()
+  {
+    $timestamp = time();
+    $header = $this->_getHeader('Received', $timestamp,
+      array(
+        array('name' => 'from', 'value' => 'mx1.googlemail.com'),
+        array('name' => 'by', 'value' => 'mail.swiftmailer.org'),
+        array('name' => 'with', 'value' => 'ESMTP'),
+        array('name' => 'for', 'value' => '<chris@swiftmailer.org>')
+        )
+      );
+    
+    $header->setPairsPerLine(2);
+    
+    $this->assertEqual(
+      'from mx1.googlemail.com by mail.swiftmailer.org' . "\r\n " .
+      'with ESMTP for <chris@swiftmailer.org>; ' . date('r', $timestamp),
+      $header->getValue()
+      );
+  }
+  
+  public function testCommentsCanBeGivenAfterNameValuePairs()
+  {
+    $timestamp = time();
+    $header = $this->_getHeader('Received', $timestamp,
+      array(
+        array(
+          'name' => 'from',
+          'value' => 'mx1.googlemail.com',
+          'comment' => 'A Gmail server'
+          ),
+        array(
+          'name' => 'by',
+          'value' => 'mail.swiftmailer.org'
+          )
+        )
+      );
+    $this->assertEqual(
+      'from mx1.googlemail.com (A Gmail server) by mail.swiftmailer.org; ' .
+      date('r', $timestamp),
+      $header->getValue()
+      );
   }
   
   // -- Private methods
   
-  private function _getHeader($name, $info = array(), $encoder = null)
+  private function _getHeader($name, $timestamp = null, $info = array(), $encoder = null)
   {
-    if (!$encoder)
-    {
-      $encoder = new Swift_Mime_MockHeaderEncoder();
-    }
     return new Swift_Mime_Header_ReceivedHeader(
-      $name, $info, $this->_charset, $encoder
+      $name, $timestamp, $info, $this->_charset, $encoder
       );
   }
   
