fe5db7853dd4831e57e0d88804eccabff115dbe5
d11wtq <d11wtq>
2008-02-05 08:02:29 +0000

    Added test coverage for Strings in PlainContentEncoder

--- tests/acceptance/Swift/Mime/ContentEncoder/PlainContentEncoderAcceptanceTest.php
+++ tests/acceptance/Swift/Mime/ContentEncoder/PlainContentEncoderAcceptanceTest.php
@@ -16,7 +16,46 @@ class Swift_Mime_ContentEncoder_PlainContentEncoderAcceptanceTest
     $this->_encoder = new Swift_Mime_ContentEncoder_PlainContentEncoder('8bit');
   }
   
-  public function testEncodingAndDecodingSamples()
+  public function testEncodingAndDecodingSamplesString()
+  {
+    $sampleFp = opendir($this->_samplesDir);
+    while (false !== $encodingDir = readdir($sampleFp))
+    {
+      if (substr($encodingDir, 0, 1) == '.')
+      {
+        continue;
+      }
+      
+      $sampleDir = $this->_samplesDir . '/' . $encodingDir;
+      
+      if (is_dir($sampleDir))
+      {
+        
+        $fileFp = opendir($sampleDir);
+        while (false !== $sampleFile = readdir($fileFp))
+        {
+          if (substr($sampleFile, 0, 1) == '.')
+          {
+            continue;
+          }
+        
+          $text = file_get_contents($sampleDir . '/' . $sampleFile);
+          $encodedText = $this->_encoder->encodeString($text);
+        
+          $this->assertEqual(
+            $encodedText, $text,
+            '%s: Encoded string should be identical to original string for sample ' .
+            $sampleDir . '/' . $sampleFile
+            );
+        }
+        closedir($fileFp); 
+      }
+      
+    }
+    closedir($sampleFp);
+  }
+  
+  public function testEncodingAndDecodingSamplesByteStream()
   {
     $sampleFp = opendir($this->_samplesDir);
     while (false !== $encodingDir = readdir($sampleFp))
