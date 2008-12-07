f38973e4998fc06fe787113049f73e96fffcc19c
d11wtq <d11wtq>
2008-12-07 08:27:06 +0000

    Implemented yay mock on ThrottlerPlugin test

--- tests/unit/Swift/Plugins/ThrottlerPluginTest.php
+++ tests/unit/Swift/Plugins/ThrottlerPluginTest.php
@@ -3,35 +3,17 @@
 require_once 'Swift/Tests/SwiftUnitTestCase.php';
 require_once 'Swift/Plugins/ThrottlerPlugin.php';
 require_once 'Swift/Events/SendEvent.php';
-require_once 'Swift/Events/CommandEvent.php';
-require_once 'Swift/Events/ResponseEvent.php';
 require_once 'Swift/Plugins/Sleeper.php';
 require_once 'Swift/Plugins/Timer.php';
 require_once 'Swift/Mime/Message.php';
-
-Mock::generate('Swift_Events_SendEvent', 'Swift_Events_MockSendEvent');
-Mock::generate('Swift_Events_CommandEvent', 'Swift_Events_MockCommandEvent');
-Mock::generate('Swift_Events_ResponseEvent', 'Swift_Events_MockResponseEvent');
-Mock::generate('Swift_Mime_Message', 'Swift_Mime_MockMessage');
-Mock::generate('Swift_Plugins_Sleeper', 'Swift_Plugins_MockSleeper');
-Mock::generate('Swift_Plugins_Timer', 'Swift_Plugins_MockTimer');
-
-class Swift_Mime_MockMessageWriter extends Swift_Mime_MockMessage {
-  public $write = array();
-  public function toByteStream(Swift_InputByteStream $is) {
-    foreach ($this->write as $chunk) {
-      $is->write($chunk);
-    }
-  }
-}
 
 class Swift_Plugins_ThrottlerPluginTest extends Swift_Tests_SwiftUnitTestCase
 {
   
   public function testBytesPerMinuteThrottling()
   {
-    $sleeper = new Swift_Plugins_MockSleeper();
-    $timer = new Swift_Plugins_MockTimer();
+    $sleeper = $this->_createSleeper();
+    $timer = $this->_createTimer();
     
     //10MB/min
     $plugin = new Swift_Plugins_ThrottlerPlugin(
@@ -39,24 +21,25 @@ class Swift_Plugins_ThrottlerPluginTest extends Swift_Tests_SwiftUnitTestCase
       $sleeper, $timer
       );
     
-    $timer->setReturnValueAt(0, 'getTimestamp', 0);
-    $timer->setReturnValueAt(1, 'getTimestamp', 1); //expected 0.6
-    $timer->setReturnValueAt(2, 'getTimestamp', 1); //expected 1.2 (sleep 1)
-    $timer->setReturnValueAt(3, 'getTimestamp', 2); //expected 1.8
-    $timer->setReturnValueAt(4, 'getTimestamp', 2); //expected 2.4 (sleep 1)
-    $sleeper->expectAt(0, 'sleep', array(1));
-    $sleeper->expectAt(1, 'sleep', array(1));
-    $sleeper->expectCallCount('sleep', 2);
+    $this->_checking(Expectations::create()
+      -> one($timer)->getTimestamp() -> returns(0)
+      -> one($timer)->getTimestamp() -> returns(1) //expected 0.6
+      -> one($timer)->getTimestamp() -> returns(1) //expected 1.2 (sleep 1)
+      -> one($timer)->getTimestamp() -> returns(2) //expected 1.8
+      -> one($timer)->getTimestamp() -> returns(2) //expected 2.4 (sleep 1)
+      -> ignoring($timer)
+      
+      -> exactly(2)->of($sleeper)->sleep(1)
+      );
     
     //10,000,000 bytes per minute
     //100,000 bytes per email
     
     // .: (10,000,000/100,000)/60 emails per second = 1.667 emais/sec
     
-    $message = new Swift_Mime_MockMessageWriter();
-    $message->write = array(str_repeat('x', 100000)); //100KB
-    $evt = new Swift_Events_MockSendEvent();
-    $evt->setReturnValue('getMessage', $message);
+    $message = $this->_createMessageWithByteCount(100000); //100KB
+    
+    $evt = $this->_createSendEvent($message);
     
     for ($i = 0; $i < 5; ++$i)
     {
@@ -67,35 +50,79 @@ class Swift_Plugins_ThrottlerPluginTest extends Swift_Tests_SwiftUnitTestCase
   
   public function testMessagesPerMinuteThrottling()
   {
-    $sleeper = new Swift_Plugins_MockSleeper();
-    $timer = new Swift_Plugins_MockTimer();
+    $sleeper = $this->_createSleeper();
+    $timer = $this->_createTimer();
     
-    //10MB/min
+    //60/min
     $plugin = new Swift_Plugins_ThrottlerPlugin(
       60, Swift_Plugins_ThrottlerPlugin::MESSAGES_PER_MINUTE,
       $sleeper, $timer
       );
     
-    $timer->setReturnValueAt(0, 'getTimestamp', 0);
-    $timer->setReturnValueAt(1, 'getTimestamp', 0); //expected 1 (sleep 1)
-    $timer->setReturnValueAt(2, 'getTimestamp', 2); //expected 2
-    $timer->setReturnValueAt(3, 'getTimestamp', 2); //expected 3 (sleep 1)
-    $timer->setReturnValueAt(4, 'getTimestamp', 4); //expected 4
-    $sleeper->expectAt(0, 'sleep', array(1));
-    $sleeper->expectAt(1, 'sleep', array(1));
-    $sleeper->expectCallCount('sleep', 2);
-    
+    $this->_checking(Expectations::create()
+      -> one($timer)->getTimestamp() -> returns(0)
+      -> one($timer)->getTimestamp() -> returns(0) //expected 1 (sleep 1)
+      -> one($timer)->getTimestamp() -> returns(2) //expected 2
+      -> one($timer)->getTimestamp() -> returns(2) //expected 3 (sleep 1)
+      -> one($timer)->getTimestamp() -> returns(4) //expected 4
+      -> ignoring($timer)
+      
+      -> exactly(2)->of($sleeper)->sleep(1)
+      );
+      
     //60 messages per minute
     //1 message per second
     
-    $message = new Swift_Mime_MockMessage();
-    $evt = new Swift_Events_MockSendEvent();
-    $evt->setReturnValue('getMessage', $message);
+    $message = $this->_createMessageWithByteCount(10);
+    
+    $evt = $this->_createSendEvent($message);
     
     for ($i = 0; $i < 5; ++$i)
     {
       $plugin->beforeSendPerformed($evt);
       $plugin->sendPerformed($evt);
+    }
+  }
+  
+  // -- Creation Methods
+  
+  private function _createSleeper()
+  {
+    return $this->_mock('Swift_Plugins_Sleeper');
+  }
+  
+  private function _createTimer()
+  {
+    return $this->_mock('Swift_Plugins_Timer');
+  }
+  
+  private function _createMessageWithByteCount($bytes)
+  {
+    $this->_bytes = $bytes;
+    $msg = $this->_mock('Swift_Mime_Message');
+    $this->_checking(Expectations::create()
+      -> ignoring($msg)->toByteStream(any()) -> calls(array($this, '_write'))
+    );
+    return $msg;
+  }
+  
+  private function _createSendEvent($message)
+  {
+    $evt = $this->_mock('Swift_Events_SendEvent');
+    $this->_checking(Expectations::create()
+      -> ignoring($evt)->getMessage() -> returns($message)
+      );
+    return $evt;
+  }
+  
+  private $_bytes = 0;
+  public function _write($invocation)
+  {
+    $args = $invocation->getArguments();
+    $is = $args[0];
+    for ($i = 0; $i < $this->_bytes; ++$i)
+    {
+      $is->write('x');
     }
   }
   
