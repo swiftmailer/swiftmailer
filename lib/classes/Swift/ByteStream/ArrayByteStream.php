ecfe7a15de2c213ebb824d29fc1ef42054b6426c
Xavier De Cock <xavier@desktop-xavier.(none)>
2009-01-29 17:37:30 +0100

    Massive Optimisation for ArrayByteStream with xdebug time passed in this file for tests 3,6s => 0,47s, all tests are fine

--- lib/classes/Swift/ByteStream/ArrayByteStream.php
+++ lib/classes/Swift/ByteStream/ArrayByteStream.php
@@ -2,12 +2,12 @@
 
 /*
  Bi-Directional ByteStream using an array in Swift Mailer.
- 
+
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -15,7 +15,7 @@
 
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- 
+
  */
 
 //@require 'Swift/InputByteStream.php';
@@ -30,21 +30,28 @@
 class Swift_ByteStream_ArrayByteStream
   implements Swift_InputByteStream, Swift_OutputByteStream
 {
-  
+
   /**
    * The internal stack of bytes.
    * @var string[]
    * @access private
    */
   private $_array = array();
-  
+
+  /**
+   * The size of the stack
+   * @var int
+   * @access private
+   */
+  private $_arraySize = 0;
+
   /**
    * The internal pointer offset.
    * @var int
    * @access private
    */
   private $_offset = 0;
-  
+
   /**
    * Create a new ArrayByteStream.
    * If $stack is given the stream will be populated with the bytes it contains.
@@ -55,6 +62,7 @@ class Swift_ByteStream_ArrayByteStream
     if (is_array($stack))
     {
       $this->_array = $stack;
+      $this->_arraySize = count($stack);
     }
     elseif (is_string($stack))
     {
@@ -65,7 +73,7 @@ class Swift_ByteStream_ArrayByteStream
       $this->_array = array();
     }
   }
-  
+
   /**
    * Reads $length bytes from the stream into a string and moves the pointer
    * through the stream by $length. If less bytes exist than are requested the
@@ -76,26 +84,24 @@ class Swift_ByteStream_ArrayByteStream
    */
   public function read($length)
   {
-    if ($this->_offset == count($this->_array))
+    if ($this->_offset == $this->_arraySize)
     {
       return false;
     }
-    
+
     // Don't use array slice
     $end = $length + $this->_offset;
-    $ret = array();
-    for ($i = $this->_offset; $i < $end; ++$i)
+    $end = $this->_arraySize<$end
+      ?$this->_arraySize
+      :$end;
+    $ret = '';
+    for (; $this->_offset < $end; ++$this->_offset)
     {
-      if (!array_key_exists($i, $this->_array))
-      {
-        break;
-      }
-      $ret[] = $this->_array[$i];
+      $ret .= $this->_array[$this->_offset];
     }
-    $this->_offset += ($i - $this->_offset); // Limit function calls
-    return implode('', $ret);
+    return $ret;
   }
-  
+
   /**
    * Writes $bytes to the end of the stream.
    * @param string $bytes
@@ -103,16 +109,15 @@ class Swift_ByteStream_ArrayByteStream
    */
   public function write($bytes, Swift_InputByteStream $is = null)
   {
-    foreach (unpack('C*', $bytes) as $byte)
-    {
-      $this->_array[] = pack('C', $byte);
-    }
+    $to_add = str_split($bytes);
+    array_unshift($to_add, &$this->_array);
+    $this->_arraySize = call_user_func_array('array_push', $to_add);
     if (isset($is))
     {
       $is->write($bytes);
     }
   }
-  
+
   /**
    * Move the internal read pointer to $byteOffset in the stream.
    * @param int $byteOffset
@@ -120,18 +125,18 @@ class Swift_ByteStream_ArrayByteStream
    */
   public function setReadPointer($byteOffset)
   {
-    if ($byteOffset > $size = count($this->_array))
+    if ($byteOffset > $this->_arraySize)
     {
-      $byteOffset = $size;
+      $byteOffset = $this->_arraySize;
     }
     elseif ($byteOffset < 0)
     {
       $byteOffset = 0;
     }
-    
+
     $this->_offset = $byteOffset;
   }
-  
+
   /**
    * Flush the contents of the stream (empty it) and set the internal pointer
    * to the beginning.
@@ -140,6 +145,7 @@ class Swift_ByteStream_ArrayByteStream
   {
     $this->_offset = 0;
     $this->_array = array();
+    $this->_arraySize = 0;
   }
-  
+
 }
