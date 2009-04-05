992afe65eb9f0d736f58e330540dd5a3b04e56da
d11wtq <chris@w3style.co.uk>
2009-04-06 09:50:09 +1000

    Check for safe_mode before passing 5th parameter to mail()

--- lib/classes/Swift/Transport/SimpleMailInvoker.php
+++ lib/classes/Swift/Transport/SimpleMailInvoker.php
@@ -45,7 +45,14 @@ class Swift_Transport_SimpleMailInvoker implements Swift_Transport_MailInvoker
    */
   public function mail($to, $subject, $body, $headers = null, $extraParams = null)
   {
-    return mail($to, $subject, $body, $headers, $extraParams);
+    if (!ini_get('safe_mode'))
+    {
+      return mail($to, $subject, $body, $headers, $extraParams);
+    }
+    else
+    {
+      return mail($to, $subject, $body, $headers);      
+    }
   }
   
 }
