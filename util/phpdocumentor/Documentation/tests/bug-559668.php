3b84f7f9f39bfa9e826e412a21ac789158e2d526
Fabien Potencier <fabien.potencier@gmail.com>
2009-09-13 22:27:05 +0800

    removed phpdocumentor from source
    Signed-off-by: Swift Mailer <chris.corbyn@swiftmailer.org>

--- util/phpdocumentor/Documentation/tests/bug-559668.php
+++
@@ -1,9 +0,0 @@
-<?php
-/** @package tests */
-/** include tests */
-require_once $FORUM['LIB'] . '/classes/db/PearDb.php';
-require PEAR . 'test' . 'me';
-include('file.ext');
-include 'file.ext';
-include(PEAR . 'test' . 'me');
-?>
