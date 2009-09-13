e6217379a705e16e66e21ccc35b458fe0fb0f9aa
Fabien Potencier <fabien.potencier@gmail.com>
2009-09-13 22:48:14 +0800

    changed all file headers to point to the LGPL license, added Chris copyright
    Signed-off-by: Swift Mailer <chris.corbyn@swiftmailer.org>

--- lib/swift_init.php
+++ lib/swift_init.php
@@ -1,21 +1,15 @@
 <?php
 
 /*
- Dependency injection initialization for Swift Mailer.
- 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+ * This file is part of SwiftMailer.
+ * (c) 2004-2009 Chris Corbyn
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
- 
+/*
+ * Dependency injection initialization for Swift Mailer.
  */
 
 //Define where the root of Swift Mailer is found
