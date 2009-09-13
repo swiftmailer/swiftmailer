5bb64b9ad1baa07212b6b225bd63410acfabf9bf
Fabien Potencier <fabien.potencier@gmail.com>
2009-09-13 23:27:54 +0800

    removed constants (they were not used except in the bootstrapping scripts)
    Signed-off-by: Swift Mailer <fabien.potencier+swiftmailer@symfony-project.org>

--- lib/swift_required.php
+++ lib/swift_required.php
@@ -12,18 +12,11 @@
  * Autoloader and dependency injection initialization for Swift Mailer.
  */
 
-//Indicate where Swift Mailer lib is found
-defined('SWIFT_LIB_DIRECTORY')
-  or define('SWIFT_LIB_DIRECTORY', dirname(__FILE__));
-
-//Path to classes inside lib
-define('SWIFT_CLASS_DIRECTORY', SWIFT_LIB_DIRECTORY . '/classes');
-
 //Load Swift utility class
-require_once SWIFT_CLASS_DIRECTORY . '/Swift.php';
+require_once dirname(__FILE__) . '/classes/Swift.php';
 
 //Start the autoloader
 Swift::registerAutoload();
 
 //Load the init script to set up dependency injection
-require_once SWIFT_LIB_DIRECTORY . '/swift_init.php';
+require_once dirname(__FILE__) . '/swift_init.php';
