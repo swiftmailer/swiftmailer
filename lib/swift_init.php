27f7671605361306194c2804a93bec8dc6f5f473
Fabien Potencier <fabien.potencier@gmail.com>
2009-09-13 17:27:54 +0200

    removed constants (they were not used except in the bootstrapping scripts)

--- lib/swift_init.php
+++ lib/swift_init.php
@@ -12,17 +12,10 @@
  * Dependency injection initialization for Swift Mailer.
  */
 
-//Define where the root of Swift Mailer is found
-defined('SWIFT_LIB_DIRECTORY')
-  or define('SWIFT_LIB_DIRECTORY', dirname(__FILE__));
-
-//Define where dependency maps can be found
-define('SWIFT_MAP_DIRECTORY', SWIFT_LIB_DIRECTORY . '/dependency_maps');
-
 //Load in dependency maps
-require_once SWIFT_MAP_DIRECTORY . '/cache_deps.php';
-require_once SWIFT_MAP_DIRECTORY . '/mime_deps.php';
-require_once SWIFT_MAP_DIRECTORY . '/transport_deps.php';
+require_once dirname(__FILE__) . '/dependency_maps/cache_deps.php';
+require_once dirname(__FILE__) . '/dependency_maps/mime_deps.php';
+require_once dirname(__FILE__) . '/dependency_maps/transport_deps.php';
 
 //Load in global library preferences
-require_once SWIFT_LIB_DIRECTORY . '/preferences.php';
+require_once dirname(__FILE__) . '/preferences.php';
