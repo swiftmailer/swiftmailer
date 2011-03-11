6a83e2f27258df62e1f5aca62f34a175805d9c01
Xavier De Cock <xdecock@gmail.com>
2011-03-11 20:17:11 +0100

    [FileSpool] Avoid Race Condition in file manipulation
    Changed the way the FileSpool Works,
    This goes lockless, by the use of atomic FS operations.
    Added a recovery operation to be able to recover files "locked" by
    zombies processes.
    Dropped the uniqid + md5 fileName generation for a complete AlphaNum Generation.

--- lib/classes/Swift/FileSpool.php
+++ lib/classes/Swift/FileSpool.php
@@ -12,11 +12,18 @@
  * Stores Messages on the filesystem.
  * @package Swift
  * @author  Fabien Potencier
+ * @author Xavier De Cock <xdecock@gmail.com>
  */
 class Swift_FileSpool extends Swift_ConfigurableSpool
 {
   /** The spool directory */
   private $_path;
+  
+  /**
+   * File WriteRetry Limit
+   * @var int
+   */
+  private $_retryLimit=10;
   
   /**
    * Create a new FileSpool.
@@ -28,7 +35,10 @@ class Swift_FileSpool extends Swift_ConfigurableSpool
     
     if (!file_exists($this->_path))
     {
-      mkdir($this->_path, 0777, true);
+      if (!mkdir($this->_path, 0777, true))
+      {
+        throw new Swift_SwiftException('Unable to create Path ['.$this->_path.']');
+      }
     }
   }
   
@@ -59,12 +69,54 @@ class Swift_FileSpool extends Swift_ConfigurableSpool
   /**
    * Queues a message.
    * @param Swift_Mime_Message $message The message to store
+   * @return boolean
    */
   public function queueMessage(Swift_Mime_Message $message)
   {
     $ser = serialize($message);
-    
-    file_put_contents($this->_path.'/'.md5($ser.uniqid()).'.message', $ser);
+    $fileName=$this->_path.'/'.$this->getRandomString(10);
+    for ($i = O; $i < $this->_retryLimit; ++$i) 
+    {
+      /* We try an exclusive creation of the file
+       * This is an atomic operation, it avoid locking mechanism
+       */
+      $fp=fopen($fileName.'.message', 'x');
+      if ($fp) 
+      {
+        $fp=fwrite($ser);
+        fclose($fp);
+        return true;
+      } 
+      else 
+      {
+        /* The file allready exists, we try a longer fileName
+         */
+        $fileName.=$this->getRandomString(1);
+      }
+    }
+    return false;
+  }
+  
+  /**
+   * Execute a recovery if for anyreason a process is sending for too long
+   * 
+   * @param int $timeout in second Defaults is for very slow smtp responses
+   */
+  public function recover($timeout=900)
+  {
+    foreach (new DirectoryIterator($this->_path) as $file)
+    {
+      $file = $file->getRealPath();
+
+      if (substr($file, -16)=='.message.sending')
+      {
+        $lockedtime=filectime($file);
+        if ((time()-$lockedtime)>$timeout) 
+        {
+          rename($file, substr($file, 0, -8));
+        }
+      }
+    }    
   }
   
   /**
@@ -89,16 +141,25 @@ class Swift_FileSpool extends Swift_ConfigurableSpool
     {
       $file = $file->getRealPath();
 
-      if (!strpos($file, '.message'))
+      if (substr($file, -8)=='.message')
       {
         continue;
       }
 
-      $message = unserialize(file_get_contents($file));
+      /* We try a rename, it's an atomic operation, and avoid locking the file */
+      if (rename($file, $file.'.sending')) 
+      {
+        $message = unserialize(file_get_contents($file.'.sending'));
 
-      $count += $transport->send($message, $failedRecipients);
+        $count += $transport->send($message, $failedRecipients);
 
-      unlink($file);
+        unlink($file.'.sending');
+      }
+      else 
+      {
+        /* This message has just been catched by another process */
+        continue;
+      }
 
       if ($this->getMessageLimit() && $count >= $this->getMessageLimit())
       {
@@ -112,5 +173,21 @@ class Swift_FileSpool extends Swift_ConfigurableSpool
     }
 
     return $count;
+  }
+  
+  /**
+   * Returns a random string needed to generate a fileName for the queue.
+   * @param int $count
+   */
+  protected function getRandomString($count) {
+    // This string MUST stay FS safe, avoid special chars
+    $base="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-.";
+    $ret='';
+    $strlen=strlen($base);
+    for ($i=0; $i<$count; ++$i) 
+    {
+      $ret.=$base[((int)rand(0,$base-1))];
+    }
+    return $ret;
   }
 }
