52d0b22b51911c02d77024648fd7e000b8d2f814
Fabien Potencier <fabien.potencier@gmail.com>
2012-03-22 13:26:13 +0100

    added a note on how to override the default temp dir used by Swiftmailer

--- lib/preferences.php
+++ lib/preferences.php
@@ -9,9 +9,9 @@
 // Sets the default charset so that setCharset() is not needed elsewhere
 Swift_Preferences::getInstance()->setCharset('utf-8');
 
-// Without these lines the default caching mechanism is "array" but this uses
-// a lot of memory.
-// If possible, use a disk cache to enable attaching large attachments etc
+// Without these lines the default caching mechanism is "array" but this uses a lot of memory.
+// If possible, use a disk cache to enable attaching large attachments etc.
+// You can override the default temporary directory by setting the TMPDIR environment variable.
 if (function_exists('sys_get_temp_dir') && is_writable(sys_get_temp_dir()))
 {
   Swift_Preferences::getInstance()
