5a6241b193c9f8365b478b90120cef3dbb2e2684
Fabien Potencier <fabien.potencier@gmail.com>
2012-06-28 10:24:43 +0200

    fixed the PEAR init script to use lazy-loading

--- lib/swift_required_pear.php
+++ lib/swift_required_pear.php
@@ -20,8 +20,5 @@ define('SWIFT_REQUIRED_LOADED', true);
 //Load Swift utility class
 require dirname(__FILE__) . '/Swift.php';
 
-//Start the autoloader
-Swift::registerAutoload();
-
-//Load the init script to set up dependency injection
-require dirname(__FILE__) . '/swift_init.php';
+//Start the autoloader and lazy-load the init script to set up dependency injection
+Swift::registerAutoload(dirname(__FILE__) . '/swift_init.php');
