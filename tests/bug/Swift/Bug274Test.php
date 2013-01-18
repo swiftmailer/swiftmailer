dcbed650ce4f9c89126f1b62757d29f9cfc749de
xdecock <xdecock@gmail.com>
2013-01-18 16:30:58 +0100

    Implemented test correctly,
    + corrected indentation

--- tests/bug/Swift/Bug274Test.php
+++ tests/bug/Swift/Bug274Test.php
@@ -1,17 +1,20 @@
 <?php
-
 require_once 'Swift/Tests/SwiftUnitTestCase.php';
-
 class Swift_Bug274Test extends Swift_Tests_SwiftUnitTestCase
 {
-	public function testEmptyFileNameAsAttachement()
-	{
-       	$message = new Swift_Message();
-		try {
-			$message->attach(Swift_Attachment::fromPath(''));
-		} catch (Swift_IoException $e) {
-			return true;
-		}
-		$this->assertFalse(true, 'Exception Not Thrown');
+    public function testEmptyFileNameAsAttachement()
+    {
+        $message = new Swift_Message();
+        $this->expectException(new Swift_IoException('File Name is empty'));
+        $message->attach(Swift_Attachment::fromPath(''));
+	}
+    public function testNonEmptyFileNameAsAttachement()
+    {
+        $message = new Swift_Message();
+        try {
+        	$message->attach(Swift_Attachment::fromPath(__FILE__));
+        } catch (Exception $e) {
+        	$this->exception($e);
+        }
 	}
 }
