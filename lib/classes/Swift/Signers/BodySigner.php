72ca7f6011b564f8bd2d35b0c8db8c6a3ec8a59f
xdecock <xdecock@gmail.com>
2013-01-23 08:04:27 +0100

    Remove need to use SignedMessage as it does not use any
    Signed Message Specific Code

--- lib/classes/Swift/Signers/BodySigner.php
+++ lib/classes/Swift/Signers/BodySigner.php
@@ -20,11 +20,11 @@ interface Swift_Signers_BodySigner extends Swift_Signer
     /**
      * Change the Swift_Signed_Message to apply the singing.
      *
-     * @param Swift_Signed_Message $message
+     * @param Swift_Message $message
      *
      * @return Swift_Signers_BodySigner
      */
-    public function signMessage(Swift_SignedMessage $message);
+    public function signMessage(Swift_Message $message);
 
     /**
      * Return the list of header a signer might tamper
