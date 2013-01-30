a2f6c046f15536a42ba5d0dba6baa06e3ce24962
xdecock <xdecock@gmail.com>
2013-01-30 11:06:59 +0100

    Modified @package on interfaces and licence correction

--- lib/classes/Swift/Signers/BodySigner.php
+++ lib/classes/Swift/Signers/BodySigner.php
@@ -12,7 +12,7 @@
  * Body Signer Interface used to apply Body-Based Signature to a message
  *
  * @package    Swift
- * @subpackage Signed
+ * @subpackage Signatures
  * @author     Xavier De Cock <xdecock@gmail.com>
  */
 interface Swift_Signers_BodySigner extends Swift_Signer
