d4d2220a12409b974d37c3b9ff8c82903bba4059
Fabien Potencier <fabien.potencier@gmail.com>
2014-07-03 13:38:37 +0200

    simplified temp dir code as sys_get_temp_dir() is always defined (min version is 5.2.4 and the function was added in 5.2.1)

--- lib/preferences.php
+++ lib/preferences.php
@@ -14,18 +14,8 @@ $preferences->setCharset('utf-8');
 // Without these lines the default caching mechanism is "array" but this uses a lot of memory.
 // If possible, use a disk cache to enable attaching large attachments etc.
 // You can override the default temporary directory by setting the TMPDIR environment variable.
-
-// The @ operator in front of is_writable calls is to avoid PHP warnings
-// when using open_basedir
-$tmp = getenv('TMPDIR');
-if ($tmp && @is_writable($tmp)) {
-    $preferences
-        ->setTempDir($tmp)
-        ->setCacheType('disk');
-} elseif (function_exists('sys_get_temp_dir') && @is_writable(sys_get_temp_dir())) {
-    $preferences
-        ->setTempDir(sys_get_temp_dir())
-        ->setCacheType('disk');
+if (@is_writable($tmpDir = sys_get_temp_dir())) {
+    $preferences->setTempDir($tmpDir)->setCacheType('disk');
 }
 
 // this should only be done when Swiftmailer won't use the native QP content encoder
