67dc83a80c7935c4b16e297120a13b9ec0858fde
Tobias Nyholm <tobias.nyholm@gmail.com>
2014-11-19 22:38:40 +0100

    Simplified the unit test and make sure to generate a new id when cloning a Swift_Message

--- tests/bug/Swift/Bug534Test.php
+++ tests/bug/Swift/Bug534Test.php
@@ -6,37 +6,30 @@ class Swift_Bug534Test extends \PHPUnit_Framework_TestCase
 {
     public function testEmbeddedImagesAreEmbedded()
     {
-        $message = Swift_Message::newInstance('Subject of mail');
-        $message->setFrom(array('from@example.com' => 'From address'));
-        $message->setTo(array('to@example.com' => 'To address'));
-
-        $image = Swift_Image::fromPath(__DIR__.'/swiftmailer.png');
-        $cid = $message->embed($image);
-        $body = 'Look at <b>this</b> image: <img alt="Embedded Image" width="181" height="68" src="'.$cid.'" />';
-        $message->setBody($body, 'text/html');
+        $message = Swift_Message::newInstance()
+            ->setFrom('from@example.com')
+            ->setTo('to@example.com')
+            ->setSubject('test')
+        ;
+        $cid = $message->embed(Swift_Image::fromPath(__DIR__.'/../../_samples/files/swiftmailer.png'));
+        $message->setBody('<img src="'.$cid.'" />', 'text/html');
 
         $that = $this;
-        $messageValidation = function ($message) use ($that) {
-            /** @var $message \Swift_Mime_Message */
-
-            preg_match("/cid:(.*)\"/", $message->toString(), $imageCIDs);
-            $imageCID = $imageCIDs[1];
-
-            preg_match("/Content-ID: <(.*)>/", $message->toString(), $imageContentIDs);
-            $imageContentID = $imageContentIDs[1];
-
-            $that->assertEquals($imageCID, $imageContentID, 'cid in body and mime part Content-ID differ');
+        $messageValidation = function (Swift_Mime_Message $message) use ($that) {
+            preg_match('/cid:(.*)"/', $message->toString(), $matches);
+            $cid = $matches[1];
+            preg_match('/Content-ID: <(.*)>/', $message->toString(), $matches);
+            $contentId = $matches[1];
+            $that->assertEquals($cid, $contentId, 'cid in body and mime part Content-ID differ');
 
             return true;
         };
 
+        $failedRecipients = array();
+
         $transport = m::mock('Swift_Transport');
         $transport->shouldReceive('isStarted')->andReturn(true);
-
-        $failedRecipients = array();
-        $transport->shouldReceive('send')
-            ->with(m::on($messageValidation), $failedRecipients)
-            ->andReturn(1);
+        $transport->shouldReceive('send')->with(m::on($messageValidation), $failedRecipients)->andReturn(1);
 
         $memorySpool = new Swift_MemorySpool();
         $memorySpool->queueMessage($message);
