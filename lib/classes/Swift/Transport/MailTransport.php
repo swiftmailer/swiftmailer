3d374ac67cea4bde2f071bd663b97cfe6d7c4fb2
Christian Schmidt <c960657@users.noreply.github.com>
2015-06-27 17:43:51 +0200

    Escape command-line args to Sendmail [#520]

--- lib/classes/Swift/Transport/MailTransport.php
+++ lib/classes/Swift/Transport/MailTransport.php
@@ -164,7 +164,7 @@ class Swift_Transport_MailTransport implements Swift_Transport
         }
 
         if ($this->_invoker->mail($to, $subject, $body, $headers,
-            sprintf($this->_extraParams, $reversePath))) {
+            sprintf($this->_extraParams, escapeshellarg($reversePath)))) {
             if ($evt) {
                 $evt->setResult(Swift_Events_SendEvent::RESULT_SUCCESS);
                 $evt->setFailedRecipients($failedRecipients);
