b8ec14799981145a1c3ee1e665bd8c710e446bd0
Ashutosh <ashutosh@sendinblue.com>
2015-10-06 18:26:58 +0530

    If egulias/email-validator bundle exists then use EmailValidator for swiftmailer to validate address in MailBoxHeader

--- lib/classes/Swift/Mime/Headers/MailboxHeader.php
+++ lib/classes/Swift/Mime/Headers/MailboxHeader.php
@@ -343,12 +343,17 @@ class Swift_Mime_Headers_MailboxHeader extends Swift_Mime_Headers_AbstractHeader
      */
     private function _assertValidAddress($address)
     {
-        if (!preg_match('/^'.$this->getGrammar()->getDefinition('addr-spec').'$/D',
-            $address)) {
-            throw new Swift_RfcComplianceException(
-                'Address in mailbox given ['.$address.
-                '] does not comply with RFC 2822, 3.6.2.'
+        if (class_exists('\Egulias\EmailValidator\EmailValidator')) {
+            $validator = new \Egulias\EmailValidator\EmailValidator();
+            if (!$validator->isValid($address)) {
+                throw new Swift_RfcComplianceException(
+                    'Address in mailbox given [' . $address . '] does not comply with RFC 2822, 3.6.2.'
                 );
+            }
+        } elseif (!preg_match('/^' . $this->getGrammar()->getDefinition('addr-spec') . '$/D', $address)) {
+            throw new Swift_RfcComplianceException(
+                'Address in mailbox given [' . $address . '] does not comply with RFC 2822, 3.6.2.'
+            );
         }
     }
 }
