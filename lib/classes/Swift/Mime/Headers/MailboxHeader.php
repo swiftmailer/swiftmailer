b0617817ce017d9f3a4fda1e42514edc6375f797
Ashutosh <ashutosh@sendinblue.com>
2015-10-06 18:33:39 +0530

    correct coding conventions

--- lib/classes/Swift/Mime/Headers/MailboxHeader.php
+++ lib/classes/Swift/Mime/Headers/MailboxHeader.php
@@ -347,12 +347,12 @@ class Swift_Mime_Headers_MailboxHeader extends Swift_Mime_Headers_AbstractHeader
             $validator = new \Egulias\EmailValidator\EmailValidator();
             if (!$validator->isValid($address)) {
                 throw new Swift_RfcComplianceException(
-                    'Address in mailbox given [' . $address . '] does not comply with RFC 2822, 3.6.2.'
+                    'Address in mailbox given ['.$address.'] does not comply with RFC 2822, 3.6.2.'
                 );
             }
-        } elseif (!preg_match('/^' . $this->getGrammar()->getDefinition('addr-spec') . '$/D', $address)) {
+        } elseif (!preg_match('/^'.$this->getGrammar()->getDefinition('addr-spec').'$/D', $address)) {
             throw new Swift_RfcComplianceException(
-                'Address in mailbox given [' . $address . '] does not comply with RFC 2822, 3.6.2.'
+                'Address in mailbox given ['.$address.'] does not comply with RFC 2822, 3.6.2.'
             );
         }
     }
