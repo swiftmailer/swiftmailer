18f52d0894c06bc7ac36a2249815798d6b6860cf
Fabien Potencier <fabien.potencier@gmail.com>
2016-04-30 11:09:19 +0200

    fixed CS

--- tests/bug/Swift/BugFileByteStreamConsecutiveReadCallsTest.php
+++ tests/bug/Swift/BugFileByteStreamConsecutiveReadCallsTest.php
@@ -9,7 +9,7 @@ class Swift_FileByteStreamConsecutiveReadCalls extends  \PHPUnit_Framework_TestC
      */
     public function shouldThrowExceptionOnConsecutiveRead()
     {
-        $fbs = new \Swift_ByteStream_FileByteStream('tralala');
+        $fbs = new \Swift_ByteStream_FileByteStream('does not exist');
         try {
             $fbs->read(100);
         } catch (\Swift_IoException $exc) {
