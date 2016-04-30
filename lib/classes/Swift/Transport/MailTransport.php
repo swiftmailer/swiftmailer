e4fae9e259726b34c3ef8d341efaf763f173e4c1
Fabien Potencier <fabien.potencier@gmail.com>
2016-04-30 15:17:19 +0200

    fixed tests

--- lib/classes/Swift/Transport/MailTransport.php
+++ lib/classes/Swift/Transport/MailTransport.php
@@ -247,9 +247,9 @@ class Swift_Transport_MailTransport implements Swift_Transport
     private function _formatExtraParams($extraParams, $reversePath)
     {
         if (false !== strpos($extraParams, '-f%s')) {
-            $extraParams = $reversePath ? sprintf($extraParams, escapeshellarg($reversePath)) : str_replace('-f%s', '', $extraParams);
+            $extraParams = empty($reversePath) ? str_replace('-f%s', '', $extraParams) : sprintf($extraParams, escapeshellarg($reversePath));
         }
 
-        return $extraParams ? null : $extraParams;
+        return !empty($extraParams) ? $extraParams : null;
     }
 }
