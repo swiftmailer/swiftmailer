b61156cda2c3aac0d3358ac8b45bac729b25d16e
Fabien Potencier <fabien.potencier@gmail.com>
2017-02-13 08:07:16 +0100

    finished #823

--- lib/classes/Swift/Transport/MailTransport.php
+++ lib/classes/Swift/Transport/MailTransport.php
@@ -170,6 +170,7 @@ class Swift_Transport_MailTransport implements Swift_Transport
             $headers = str_replace("\r\n.", "\r\n..", $headers);
             $subject = str_replace("\r\n.", "\r\n..", $subject);
             $body = str_replace("\r\n.", "\r\n..", $body);
+            $to = str_replace("\r\n.", "\r\n..", $to);
         }
 
         if ($this->_invoker->mail($to, $subject, $body, $headers, $this->_formatExtraParams($this->_extraParams, $reversePath))) {
