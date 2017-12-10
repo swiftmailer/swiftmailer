ab3608bedace016d44996a616a03b76e425d1ddf
Gabriel Caruso <carusogabriel34@gmail.com>
2017-12-10 05:54:05 -0200

    Use Null Coalesce Operator

--- tests/acceptance/Swift/Transport/StreamBuffer/BasicSocketAcceptanceTest.php
+++ tests/acceptance/Swift/Transport/StreamBuffer/BasicSocketAcceptanceTest.php
@@ -19,7 +19,7 @@ class Swift_Transport_StreamBuffer_BasicSocketAcceptanceTest extends Swift_Trans
     {
         $parts = explode(':', SWIFT_SMTP_HOST);
         $host = $parts[0];
-        $port = isset($parts[1]) ? $parts[1] : 25;
+        $port = $parts[1] ?? 25;
 
         $this->buffer->initialize(array(
             'type' => Swift_Transport_IoBuffer::TYPE_SOCKET,
