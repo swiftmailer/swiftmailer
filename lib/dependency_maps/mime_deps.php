b737cebf1624e392d5aaa265672c543f976aab0b
Christian Schmidt <cs@firmafon.dk>
2018-01-25 20:08:09 +0100

    Add Swift_DependencyContainer::asArray()

--- lib/dependency_maps/mime_deps.php
+++ lib/dependency_maps/mime_deps.php
@@ -64,12 +64,12 @@ Swift_DependencyContainer::getInstance()
     ->register('mime.headerfactory')
     ->asNewInstanceOf('Swift_Mime_SimpleHeaderFactory')
     ->withDependencies([
-            'mime.qpheaderencoder',
-            'mime.rfc2231encoder',
-            'email.validator',
-            'properties.charset',
-            'mime.addressencoder',
-        ])
+        'mime.qpheaderencoder',
+        'mime.rfc2231encoder',
+        'email.validator',
+        'properties.charset',
+        'mime.addressencoder',
+    ])
 
     ->register('mime.headerset')
     ->asNewInstanceOf('Swift_Mime_SimpleHeaderSet')
