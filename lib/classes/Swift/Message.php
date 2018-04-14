389af9207cfe18e6aa9ba7d9346f1390c104ca93
Michael Babker <michael.babker@gmail.com>
2018-04-13 19:16:19 -0500

    Correct doc blocks of signer methods

--- lib/classes/Swift/Message.php
+++ lib/classes/Swift/Message.php
@@ -75,7 +75,7 @@ class Swift_Message extends Swift_Mime_SimpleMessage
     }
 
     /**
-     * Detach a signature handler from a message.
+     * Attach a new signature handler to the message.
      *
      * @return $this
      */
@@ -91,7 +91,7 @@ class Swift_Message extends Swift_Mime_SimpleMessage
     }
 
     /**
-     * Attach a new signature handler to the message.
+     * Detach a signature handler from a message.
      *
      * @return $this
      */
